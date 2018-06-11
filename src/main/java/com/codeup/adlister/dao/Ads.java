d865323f98fd1d29f6b68556120070e5eedbd0bf
John <cooks.johns@gmail.com>
2018-06-11 11:17:47 -0500

    servlet is done but i needed to follow add on to Ads.java next jsp

--- src/main/java/com/codeup/adlister/dao/Ads.java
+++ src/main/java/com/codeup/adlister/dao/Ads.java
@@ -1,6 +1,7 @@
 package com.codeup.adlister.dao;
 
 import com.codeup.adlister.models.Ad;
+import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
 
 import java.util.List;
 
@@ -9,4 +10,13 @@ public interface Ads {
     List<Ad> all();
     // insert a new ad and return the new ad's id
     Long insert(Ad ad);
+
+    // for searching Ads =>
+
+    List<Ad> getAdsByTerm(String search);
+
+    List<Ad> byUsername(BIConversion.User user);
+    Ad getAdById(int findId);
+
+    // end of add search
 }
