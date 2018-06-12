5cdf2a6e99bc5ce10e00d0f44c706eabd3ee53d3
unfaiyted <unfaiyted@gmail.com>
2018-06-12 08:24:54 -0500

    added categories to migration file, clean up ad categories dao

--- src/main/java/com/codeup/adlister/dao/MySQLAdCategoriesDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdCategoriesDao.java
@@ -84,7 +84,6 @@ public class MySQLAdCategoriesDao implements AdCategories {
                     "SELECT  c.id,\n" +
                     "        coalesce(c.main_id, c1.main_id) AS main_id,\n" +
                     "        c.title AS title\n" +
-                    "\n" +
                     "FROM categories c\n" +
                     " LEFT OUTER JOIN categories c1 ON (c1.id = c.main_id)\n" +
                     "ORDER BY coalesce(c1.title, c.title), c1.main_id\n");
