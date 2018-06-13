085f6b8e8c28e96b6d5188ba8eb67652ce3f747d
John <cooks.johns@gmail.com>
2018-06-13 10:56:54 -0500

    fixed my merge conflicts

--- src/main/java/com/codeup/adlister/controllers/SearchAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/SearchAdServlet.java
@@ -15,29 +15,15 @@ public class SearchAdServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 
-        // Render Search Page with Search function on it
-
-        // if param search ! =null then show results of search
-
-                // Ok not null, lets do the search
-                // Search reached out to DAOFactory, runs a search function against that value provided by search
-            // returns value and appends to page
-
-        // Search is displayed on page
-
-
-            String input = request.getParameter("term");
+        String input = request.getParameter("term");
         System.out.println(input);
 
-            if (input != null) {
+        if (input != null) {
 
-                request.setAttribute("term", input);
-                request.setAttribute("ads", DaoFactory.getAdsDao().search(input));
-            }
-                request.getRequestDispatcher("/WEB-INF/ads/search.jsp").forward(request, response);
-
+            request.setAttribute("term", input);
+            request.setAttribute("ads", DaoFactory.getAdsDao().search(input));
         }
-        // what happend with the submit and what going on with the search
-
+        request.getRequestDispatcher("/WEB-INF/ads/search.jsp").forward(request, response);
 
     }
+}
